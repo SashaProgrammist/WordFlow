704c1dc17de7553039f81e6d7db5c42debb80248
SashaProgrammist <49712254+SashaProgrammist@users.noreply.github.com>
2024-12-06 17:13:26 +0300

    Reset WordSeed

---
+++ WordSeed.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+
+class WordSeed:
+    def __init__(self, name_db):
+        self.conn = sqlite3.connect(name_db)
+        self.cursor = self.conn.cursor()
+
+        self.conn.commit()
+
+    def __del__(self):
+        self.conn.close()
