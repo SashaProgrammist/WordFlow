3763593ae9593660c4637d861aecdd9f5e1140cf
SashaProgrammist <49712254+SashaProgrammist@users.noreply.github.com>
2024-12-06 18:50:26 +0300

    add button

--- mainwindow.py
+++ mainwindow.py
@@ -1,7 +1,8 @@
 # This Python file uses the following encoding: utf-8
 import sys
 
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog
+from PySide6.QtCore import Slot
 
 # Important:
 # You need to run the following command to generate the ui_form.py file
@@ -9,12 +10,21 @@ from PySide6.QtWidgets import QApplication, QMainWindow
 #     pyside2-uic form.ui -o ui_form.py
 from ui_form import Ui_MainWindow
 
+from WordSeed import WordSeed
+
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.say_hello)
 
+        self.wordSead = WordSeed("data/test.db")
+
+    @Slot()
+    def say_hello(self):
+        wb_patch = QFileDialog.getOpenFileName()[0]
+        print(wb_patch)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
