c759c339baa2a354e03477e06e920c54fb1c51cf
SashaProgrammist <49712254+SashaProgrammist@users.noreply.github.com>
2024-12-09 12:24:52 +0300

    simpl displey result

--- FlowControl.py
+++ FlowControl.py
@@ -1,8 +1,9 @@
 import sqlite3
+from random import choice
 
 
 class FlowControl:
-    def __init__(self, name_db):
+    def __init__(self, name_db : str):
         self.conn = sqlite3.connect(name_db)
         self.cursor = self.conn.cursor()
 
@@ -15,6 +16,39 @@ class FlowControl:
         """).fetchall()
 
         return [lang[0] for lang in languages]
+
+    def getAnyPhrase(self, language: str):
+        result = self.cursor.execute(f"""
+        SELECT id, phrase_set_id, phrase
+        FROM phrases
+        WHERE language='{language}';
+        """).fetchall()
+
+        result = choice(result)
+
+        result = {
+            "id" : result[0],
+            "phrase_set_id" : result[1],
+            "phrase" : result[2]
+        }
+
+        return result
+
+    def getAnyTranslate(self, language: str, set_id: int):
+        result = self.cursor.execute(f"""
+        SELECT id, phrase, language
+        FROM phrases
+        WHERE phrase_set_id={set_id} AND language!='{language}';
+        """).fetchone()
+
+        result = {
+            "id" : result[0],
+            "phrase" : result[1],
+            "language" : result[2]
+        }
+
+        return result
+
 
     def __del__(self):
         self.conn.close()
--- mainwindow.py
+++ mainwindow.py
@@ -1,8 +1,8 @@
 # This Python file uses the following encoding: utf-8
 import sys
 
-from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog
-from PySide6.QtCore import Slot
+from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox
+from PySide6.QtCore import Slot, QTimer
 
 # Important:
 # You need to run the following command to generate the ui_form.py file
@@ -22,11 +22,17 @@ class MainWindow(QMainWindow):
 
         self.ui.buttonApdateDB.clicked.connect(self.abdateDB)
         self.ui.buttonChoiceLeng.clicked.connect(self.open_popup)
+        self.ui.buttonContinue.clicked.connect(self.mainProcess)
+
+        self.timer = QTimer(self)
+        # self.timer.
 
         self.wordSead = WordSeed("data/test.db")
         self.flowControl = FlowControl("data/test.db")
 
         self.curLeng = None
+        self.curPhrase = None
+        self.curTrans = None
 
     @Slot()
     def abdateDB(self):
@@ -38,6 +44,20 @@ class MainWindow(QMainWindow):
         popup = LangChoicePopupWindow(self.flowControl.getAllLanguages(), self)
         if popup.exec():
             self.curLeng = popup.selected_language
+
+    @Slot()
+    def mainProcess(self):
+        if self.curLeng is None:
+            QMessageBox.warning(self, "Warning", "Please select a language!")
+            return
+
+        self.curPhrase = self.flowControl.getAnyPhrase(self.curLeng)
+
+        self.ui.labelOriginalText.setText(self.curPhrase["phrase"])
+
+        self.curTrans = self.flowControl.getAnyTranslate(self.curLeng, self.curPhrase["phrase_set_id"])
+
+        self.ui.labelTranslationText.setText(self.curTrans["phrase"])
 
 
 if __name__ == "__main__":
